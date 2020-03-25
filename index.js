78236bf5aa1297f94e586f2024bef018cda86091
Reid Watson <j.watsonreid@gmail.com>
2020-03-25 19:31:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay (arrayMusicians,arrayInstruments){
-  //var i = 0
+  var i = 0
   var array = []
   for (i = 0; i < arrayMusicians.length; i++){
     array.push(arrayMusicians[i] + " plays " + arrayInstruments[i]);
