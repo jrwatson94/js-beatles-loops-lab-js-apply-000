32e23d477a45bd998cb3a99b69999208c4854ae4
Reid Watson <j.watsonreid@gmail.com>
2020-03-25 19:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay (arrayMusicians,arrayInstruments){
   var i = 0
   var array = []
   for (i = 0; i < arrayMusicians.length; i++){
-    array.push(arrayMusicians[i] + " " + arrayInstruments[i]);
+    array.push(arrayMusicians[i] + " plays " + arrayInstruments[i]);
   }
   
   return array
